78f52a3a88f37e09bb40b594da4fde68cd1d0790
nea14e <nea14e@gmail.com>
2025-08-12 13:21:31 +0300

    Простой запрос - Комментарии в коде

--- Backend/Backend/ApiControllers/SimpleRequestApiController.cs
+++ Backend/Backend/ApiControllers/SimpleRequestApiController.cs
@@ -9,6 +9,7 @@ public class SimpleRequestApiController : ControllerBase
     [HttpGet("get")]
     public IActionResult Get()
     {
+        // Поздравляем! Вы нашли метод в ApiController, который вызывается по кнопке из "Простой запрос-2"
         // Строка по умолчанию не может распарситься в JSON. Поэтому тут JsonResult, а не string
         return new JsonResult("Привет, мир!");
     }
