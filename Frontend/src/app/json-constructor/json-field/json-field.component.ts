3492c42c4468a14fa67ffe0d22dd6057f3965660
nea14e <nea14e@gmail.com>
2025-09-02 15:39:28 +0300

    Редактор JSON - Просмотр значения JSON. Переделал объект со словаря на список, т.к. ключи в словаре дублировали названия полей, вводимых пользователем на форме. TODO json-списки

--- Frontend/src/app/json-constructor/json-field/json-field.component.ts
+++ Frontend/src/app/json-constructor/json-field/json-field.component.ts
@@ -1,8 +1,17 @@
 import {Component, inject, input} from '@angular/core';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 import {FieldType} from './models/json-field-type';
 import {MatFormField, MatInput} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
+import {JsonConstructorService} from '../services/json-constructor.service';
 
 @Component({
   selector: 'app-json-field',
@@ -21,6 +30,8 @@ export class JsonFieldComponent {
 
   private formBuilder = inject(FormBuilder);  // До вызова buildForm()
   form = input<FormGroup>(this.buildForm());
+  private service = inject(JsonConstructorService);
+
   readonly FieldType = FieldType;
   readonly Object = Object;
 
@@ -46,10 +57,10 @@ export class JsonFieldComponent {
           newControl = new FormControl({value: null, disabled: true});
           break;
         case FieldType.object:
-          newControl = this.formBuilder.group({
-            id: this.buildForm('id', FieldType.number, 111),
-            title: this.buildForm('title', FieldType.string, 'Qwerty')
-          });
+          newControl = this.formBuilder.array([
+            this.buildForm('id', FieldType.number, 111),
+            this.buildForm('title', FieldType.string, 'Qwerty')
+          ]);
           break;
         case FieldType.list:
           newControl = this.formBuilder.array([
@@ -76,16 +87,37 @@ export class JsonFieldComponent {
   }
 
   get objectInnerFields() {
-    const objectFormGroup = this.form().get('fieldValue')! as FormGroup;
-    console.log('objectFormGroup:', objectFormGroup);
-    console.log('objectFormGroup.controls:', objectFormGroup.controls);
-    console.log('objectFormGroup.controls.keys():', Object.keys(objectFormGroup.controls));
-    return objectFormGroup.controls;
+    const innerForm = this.form().get('fieldValue')! as FormArray;
+    console.log('innerForm:', innerForm);
+    console.log('innerForm.controls:', innerForm.controls);
+    return innerForm.controls as FormGroup[];
   }
 
-  getObjectInnerField(fieldName: string) {
-    const fieldFormGroup = this.objectInnerFields[fieldName] as FormGroup;
-    console.log(`objectFormGroup.controls.keys(${fieldName}):`, fieldFormGroup);
-    return fieldFormGroup;
+  async onEyeClick() {
+    const totalValue = this.getTotalValue(this.form());
+    console.log('totalValue:', totalValue);
+    const jsonValue = JSON.stringify(totalValue);
+    const formattedJsonValue = await this.service.prettify(jsonValue);
+    console.log('formattedJsonValue:', formattedJsonValue);
+  }
+
+  getTotalValue(form: FormGroup) {
+    const valueControl = form.get('fieldValue')!;
+    const type = form.get('fieldType')!.value as FieldType;
+
+    if (type === FieldType.object) {
+      let value: any = {};
+      const fieldFormGroups = (valueControl as FormArray).controls as FormGroup[];
+      for (let fieldFormGroup of fieldFormGroups) {
+        const fieldName = fieldFormGroup.get('fieldName')!.value as string;
+        value[fieldName] = this.getTotalValue(fieldFormGroup);
+      }
+      return value;
+    } else if (type === FieldType.list) {
+      // TODO
+    } else if (type === FieldType.null) {
+      return null;
+    }
+    return valueControl.value;
   }
 }
