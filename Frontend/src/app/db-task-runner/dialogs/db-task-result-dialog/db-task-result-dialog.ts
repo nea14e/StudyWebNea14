d801bb6f3e06ff692799aa93d5713a4cea9789c8
nea14e <nea14e@gmail.com>
2025-10-01 15:28:11 +0300

    Система SQL-задач - Диалог с результатом задачи

--- Frontend/src/app/db-task-runner/dialogs/db-task-result-dialog/db-task-result-dialog.ts
+++ Frontend/src/app/db-task-runner/dialogs/db-task-result-dialog/db-task-result-dialog.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, ElementRef, inject, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {DbTaskResultHelper} from '../../helpers/db-task-result-helper';
 import {DbTaskItem} from '../../models/db-task-item';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
@@ -12,11 +12,16 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 export class DbTaskResultDialog implements OnInit {
   dialogRef = inject(MatDialogRef<DbTaskResultDialog>);
   task = inject<DbTaskItem>(MAT_DIALOG_DATA);
-
+  renderer = inject(Renderer2);
   helper = inject(DbTaskResultHelper);
 
+  @ViewChild('resultView', {static: true})
+  resultView: ElementRef | null = null;
+
   ngOnInit(): void {
-
+    const resultHtml = this.helper.createInnerContent(this.task, this.renderer);
+    if (!this.resultView)
+      throw new Error('Не найден родительский элемент в диалоге результата');
+    this.renderer.appendChild(this.resultView.nativeElement, resultHtml);
   }
-
 }
