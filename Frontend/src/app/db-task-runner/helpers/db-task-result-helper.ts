e794e1749cb592603c071d474ef1f04e8ef0e5cb
nea14e <nea14e@gmail.com>
2025-10-01 13:56:34 +0300

    Система SQL-задач - Вынес общую логику всплывающей подсказки и будущего диалога в DbTaskResultHelper

---
+++ Frontend/src/app/db-task-runner/helpers/db-task-result-helper.ts
@@ -0,0 +1,85 @@
+import {Injectable, Renderer2} from '@angular/core';
+import {DbTaskItem} from '../models/db-task-item';
+import {DbTaskItemState} from '../models/db-task-item-state';
+import {DbTaskItemType} from '../models/db-task-item-type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbTaskResultHelper {
+  createInnerContent(task: DbTaskItem, renderer: Renderer2): any {
+    const containerEl = renderer.createElement('div');
+    renderer.addClass(containerEl, 'container-fluid');
+
+    this.createStateInfo(task, renderer, containerEl);
+    this.createResultInfo(task, renderer, containerEl);
+
+    return containerEl;
+  }
+
+  private createStateInfo(task: DbTaskItem, renderer: Renderer2, containerEl: any) {
+    const stateRow = renderer.createElement('div');
+    renderer.addClass(stateRow, 'row');
+    {
+      const state = this.getState(task);
+      const stateEl = renderer.createText(state);
+      renderer.appendChild(stateRow, stateEl);
+    }
+    renderer.appendChild(containerEl, stateRow);
+  }
+
+  private getState(task: DbTaskItem) {
+    switch (task.state) {
+      case DbTaskItemState.NotStarted:
+        return 'Ещё не начато';
+      case DbTaskItemState.Running:
+        return 'Выполняется';
+      case DbTaskItemState.Completed:
+        return 'Успешно завершено';
+      case DbTaskItemState.Error:
+        return 'Ошибка: ' + task.exceptionMessage;
+    }
+  }
+
+  private createResultInfo(task: DbTaskItem, renderer: Renderer2, containerEl: any) {
+    if (task.state === DbTaskItemState.Completed && task.result !== null) {
+      const resultRow = renderer.createElement('div');
+      renderer.addClass(resultRow, 'row');
+      if (task.type === DbTaskItemType.Scalar) {
+        const scalarValue = task.result[0][0];
+        const scalarValueEl = renderer.createText(scalarValue?.toString() ?? 'null');
+        renderer.appendChild(resultRow, scalarValueEl);
+      } else if (task.type === DbTaskItemType.Table) {
+        const table = renderer.createElement('table');
+        renderer.addClass(table, 'table');
+        renderer.addClass(table, 'table-bordered');
+        renderer.addClass(table, 'table-striped');
+        renderer.addClass(table, 'table-hover');
+        {
+          const tbody = renderer.createElement('tbody');
+          {
+            for (let i = 0; i < task.result.length; i++) {
+              const tr = renderer.createElement('tr');
+              for (let j = 0; j < task.result[i].length; j++) {
+                const td = renderer.createElement('td');
+                {
+                  const value = task.result[i][j]?.toString() ?? 'null';
+                  const valueEl = renderer.createText(value);
+                  renderer.appendChild(td, valueEl);
+                }
+                renderer.appendChild(tr, td);
+              }
+              renderer.appendChild(tbody, tr);
+            }
+          }
+          renderer.appendChild(table, tbody);
+        }
+        renderer.appendChild(resultRow, table);
+      } else {
+        const stubEl = renderer.createText('Данный тип задач не возвращает значение.');
+        renderer.appendChild(resultRow, stubEl);
+      }
+      renderer.appendChild(containerEl, resultRow);
+    }
+  }
+}
